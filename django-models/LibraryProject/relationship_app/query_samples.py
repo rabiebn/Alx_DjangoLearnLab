2e58079d449c9967f7e03887e96b59d8f22a1915
rabiebn <rabie.bnmr@gmail.com>
2025-09-15 13:03:57 +0100

    0.2 changed var name

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,13 +4,14 @@ from .models import Book, Author, Library, Librarian
 author1 = Author(name="J. K. Rowling")
 book = Book(title="Harry Potter", author=author1.name)
 lib = Library(name="City Library", books=[])
-librarian = Librarian(name="Mohammed", library=lib.name)
+librarian = Librarian(name="Mohammed", library=lib)
 
 #Query all books by a specific author
 books_by_author = Book.objects.filter(author=author1)
 
 #List all books in a library
-all_books_in_lib = lib.books.all()
+library_name = lib.name
+all_books_in_lib = Library.objects.get(name=library_name)
 
 #Retrieve the librarian for a library
 librarian = Librarian.objects.filter(library=lib.name)
